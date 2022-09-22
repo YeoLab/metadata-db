4ec8dad625041f6ac30235c76444774f68564314
Brian Yee <bay001@health.ucsd.edu>
2022-09-22 13:35:30 -0700

    add static files and more migrations

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Post, CLIP, Fastq
+from .models import Post, CLIPManifest, Fastq
 
 # make model visible on the admin page
 admin.site.register(Post)
-admin.site.register(CLIP)
+admin.site.register(CLIPManifest)
 admin.site.register(Fastq)
 
 # Register your models here.
--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+import os
 
 
 class Post(models.Model):
@@ -21,44 +22,79 @@ class Post(models.Model):
         return self.title
 
 
-class CLIP(models.Model):
-
-    bar_choices = [
-        ("barcode_set1.csv", "barcode_set1.csv"),
-    ]
-    chrom_choices = [
-        ("hg38.chrom.sizes", "hg38.chrom.sizes"),
-        ("hg19.chrom.sizes", "hg19.chrom.sizes"),
+class CLIPManifest(models.Model):
+    barcode_choices = [
+        (
+            "/projects/ps-yeolab4/software/eclip/0.7.1/examples/inputs/InvRiL19_adapters.fasta",
+            "InvRiL19_adapters.fasta"
+        ),
+        (
+            "/projects/ps-yeolab4/software/eclip/0.7.1/examples/inputs/InvRNA1_adapters.fasta",
+            "InvRNA1_adapters.fasta"
+        ),
+        (
+            "/projects/ps-yeolab4/software/eclip/0.7.1/examples/inputs/InvRNA2_adapters.fasta",
+            "InvRNA2_adapters.fasta"
+        ),
+        (
+            "/projects/ps-yeolab4/software/eclip/0.7.1/examples/inputs/InvRNA3_adapters.fasta",
+            "InvRNA3_adapters.fasta"
+        ),
+        (
+            "/projects/ps-yeolab4/software/eclip/0.7.1/examples/inputs/InvRNA4_adapters.fasta",
+            "InvRNA4_adapters.fasta"
+        ),
     ]
     star_choices = [
-        ("star_2_7_gencode29_sjdb", "star_2_7_gencode29_sjdb"),
-        ("star_2_7_6a_release6_sjdb", "star_2_7_6a_release6_sjdb"),
+        (
+            "/projects/ps-yeolab4/software/eclip/0.7.1/examples/inputs/star_2_7_gencode40_sjdb/",
+            "star_2_7_gencode40_sjdb/ (GRCh38)"
+        ),
+        (
+            "/projects/ps-yeolab4/software/eclip/0.7.1/examples/inputs/star_2_7_6a_gencode19_sjdb/",
+            "star_2_7_6a_gencode19_sjdb/ (hg19)"
+        ),
     ]
+    chrom_choices = []
+    for tuple in star_choices:
+        chrom_choices.append((os.path.join(tuple[0], 'chrNameLength.txt'), tuple[1] + " (must match genome)"))
+    repeat_choices = [
+        (
+            "/projects/ps-yeolab4/software/eclip/0.7.1/examples/inputs/star_2_7_homo_sapiens_repbase_fixed_v2",
+            "star_2_7_homo_sapiens_repbase_fixed_v2"
+        ),
+        (
+            "/projects/ps-yeolab3/bay001/annotations/RepBase18.05/star_2_7_mus_musculus_repbase_fixed_v2",
+            "star_2_7_mus_musculus_repbase_fixed_v2"
+        )
+    ]
+    species_choices = [('hg19', 'hg19'), ('GRCh38_v40', 'GRCh38_v40')]
 
-    description = models.TextField(max_length=200, blank=True)
-    barcode_file = models.CharField(max_length=25, choices=bar_choices, blank=True)
-    chrom_sizes = models.CharField(max_length=50, choices=chrom_choices, blank=True)
-    star_index = models.CharField(max_length=50, choices=star_choices, blank=True)
-    umi_pattern = models.CharField(max_length=10, default="NNNNNNNNNN")
+    description = models.CharField(max_length=200, blank=True)
+    species = models.CharField(max_length=20, choices=species_choices, blank=True)
+    repeat_index = models.CharField(max_length=120, choices=repeat_choices, blank=True)
+    star_index = models.CharField(max_length=90, choices=star_choices, blank=True)
+    chrom_sizes = models.CharField(max_length=120, choices=chrom_choices, blank=True)
+
+    umi_pattern = models.CharField(max_length=20, default="NNNNNNNNNN")
     # set default: 10 Ns
     fastqs = models.CharField(max_length=200, blank=True)
-
+    barcode_file = models.CharField(max_length=120, choices=barcode_choices, blank=True)
     def __str__(self):
-        return self.barcode_file
+        return self.description[:100] + "..."
 
 
 class Fastq(models.Model):
 
-    adapt_choices = [
-        ("InvRiL19_adapters.fasta", "InvRiL19_adapters.fasta"),
-        ("InvRNA1.fasta", "InvRNA1.fasta"),
-        ("InvRNA2.fasta", "InvRNA2.fasta"),
-    ]
+    ip_title = models.CharField(max_length=100)
+    ip_path = models.CharField(max_length=100)
+    ip_adapter_path = models.CharField(max_length=90)
+    ip_complete = models.BooleanField()
 
-    title = models.CharField(max_length=100)
-    path = models.CharField(max_length=100)
-    adapter_path = models.CharField(max_length=50, choices=adapt_choices, blank=True)
-    complete = models.BooleanField()
+    sminput_title = models.CharField(max_length=100)
+    sminput_path = models.CharField(max_length=100)
+    sminput_adapter_path = models.CharField(max_length=90)
+    sminput_complete = models.BooleanField()
 
     def __str__(self):
-        return self.title
+        return self.ip_title
