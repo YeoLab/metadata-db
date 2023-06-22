39fbb4fed2de2298cdcd0e3e91cfa7f2bba31be2
Katherine Wong <katmwong29@gmail.com>
2023-06-22 15:17:56 -0700

    add comments to views.py

--- form_builder/views.py
+++ form_builder/views.py
@@ -14,22 +14,31 @@ import os
 @csrf_exempt
 @login_required
 def CLIP_form(request):
+    '''
+    Generates the view for the eCLIP form builder.
+    Args:
+        request: HttpRequest
+    Returns:
+        HttpResponse
+    '''
+    # generate list of selected files and existing SEfastqs 
     files = []
     try:
         SEfastqs = SingleEndFastq.objects.filter(submitter=request.user)
         for key in request.GET.keys():
                 if key.startswith('file'):
                     files.append(os.path.join(base_url, request.GET.get(key)))
-
+    # error handling
     except Exception as e:
         print(e)
         SEfastqs = []
-
+    # form submission
     if request.method == 'POST':
         form = CLIPManifestForm(request.POST)
         data = make_CLIP_form(form, request)
         if data:
             return data
+    # get globus file manager url
     else:
         if request.GET.get('endpoint'):
             base_url = '/projects/ps-yeolab5/seqdata' + request.GET.get('path')
@@ -42,7 +51,8 @@ def CLIP_form(request):
         'form': form,
         'SEfastqs': SEfastqs,
         'files': files,
-        'globus_url': "https://app.globus.org/file-manager?method=GET&origin_id=d9358457-3f23-4b35-bbae-68d2f4190545&origin_path=%2F&action="+ settings.DOMAIN
+        'globus_url': 
+        "https://app.globus.org/file-manager?method=GET&origin_id=d9358457-3f23-4b35-bbae-68d2f4190545&origin_path=%2F&action="+ settings.DOMAIN
     }
     return render(request, 'form_builder/CLIP_form.html', context=context)
 
@@ -161,6 +171,13 @@ def rnaseqPE_yaml(rnaseq):
 '''
 
 def index_view(request):
+    '''
+    Globus authenticator.
+    Args:
+        request: HttpRequest
+    Return:
+        HttpResponse
+    '''
     context = {'LOGIN_URL': settings.LOGIN_URL}
     template = 'form_builder/index.html'
     return render(request, template, context)
@@ -168,7 +185,13 @@ def index_view(request):
 
 @csrf_exempt
 def receive_data(request):
+    '''
+    Print POST data.
+    Args:
+        request: HttpRequest
+    Return:
+        HttpResponse
+    '''
     print('post', request.POST)
-
     context = {}
     return render(request, 'form_builder/receive-data.html', context)
